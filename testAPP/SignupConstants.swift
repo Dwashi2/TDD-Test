e28d1154ad4682cfc8315094590dd793857e7b00
Daniel Washington <danielvertigo@hotmail.com>
2022-11-22 21:06:28 -0300

    TDD test

---
+++ testAPP/SignupConstants.swift
@@ -0,0 +1,13 @@
+//
+//  SignupConstants.swift
+//  testAPP
+//
+//  Created by Daniel Washington Ignacio on 17/11/22.
+//
+
+import Foundation
+
+struct SignupConstants {
+    static let firstNameMinLength: Int = 2
+    static let firstNameMaxLength: Int = 10
+}
